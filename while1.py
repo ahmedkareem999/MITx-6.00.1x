3293d8c4d8ca7d69c9ed763332c272905b4aa375
Kareem Ahmed Sheik <ahmedkareem999@gmail.com>
2017-06-23 20:28:40 +0200

    week1-Exercise:while1

---
+++ while1.py
@@ -0,0 +1,17 @@
+'''
+In this problem you'll be given a chance to practice writing some while loops.
+
+1. Convert the following into code that uses a while loop.
+
+print 2
+prints 4
+prints 6
+prints 8
+prints 10
+prints Goodbye!
+'''
+n = 2
+while ( n >= 2 and n <=10):
+    print(n)
+    n += 2
+print("Goodbye!")
