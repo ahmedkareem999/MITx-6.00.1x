79c35c4d111ad8245b7cecbbd8c2f171a119cb66
Kareem Ahmed Sheik <ahmedkareem999@gmail.com>
2017-06-23 20:29:40 +0200

    Week1-Exercise:while2

---
+++ while2.py
@@ -0,0 +1,15 @@
+'''
+Convert the following into code that uses a while loop.
+
+prints Hello!
+prints 10
+prints 8
+prints 6
+prints 4
+prints 2
+'''
+n = 10
+print ("Hello!")
+while n <= 10 and n >= 2:
+    print (n)
+    n -= 2
