d51699474a9221a71daff29d7f834bedf618acad
Kareem Ahmed Sheik <ahmedkareem999@gmail.com>
2017-06-23 20:26:54 +0200

    week1-Exercise:varA varB
    Write a piece of Python code that evaluates varA and varB. then prints out one of the following messages

---
+++ varAvarB.py
@@ -0,0 +1,21 @@
+'''
+Assume that two variables, varA and varB, are assigned values, either numbers or strings.
+
+Write a piece of Python code that evaluates varA and varB, and then prints out one of the following messages:
+
+"string involved" if either varA or varB are strings
+
+"bigger" if varA is larger than varB
+
+"equal" if varA is equal to varB
+
+"smaller" if varA is smaller than varB
+'''
+if type(varA) == str or type(varB) == str:
+    print ("string involved")
+elif varA > varB:
+    print("bigger")
+elif varA == varB:
+    print("equal")
+elif varA < varB:
+    print("smaller")
