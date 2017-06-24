35e06378da52e19ecc773f0d6e2003105eb33a6e
Kareem Ahmed Sheik <ahmedkareem999@gmail.com>
2017-06-24 14:09:49 +0200

    Week-2:Exercise-Square

---
+++ square.py
@@ -0,0 +1,13 @@
+'''
+Week-2:Exercise-Square
+Write a Python function, square, that takes in one number and returns the square of that number.
+
+This function takes in one number and returns one number.
+'''
+#code
+def square(x):
+    '''
+    x: int or float.
+    '''
+    # Your code here
+    return x * x
